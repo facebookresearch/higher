ba26e50399bb1174762096ffca00e6d0ed737f9b
Edward Grefenstette <egrefen@users.noreply.github.com>
2020-02-26 17:39:11 +0000

    Add support for flattened parameters in torch.nn.RNN-like modules.

--- higher/patch.py
+++ higher/patch.py
@@ -282,9 +282,23 @@ def _make_functional(
             params_box[0][params_offset:params_offset + num_params]
         ):
             setattr(self, name, param)
+
+        # This snippet deals with torch.nn.{RNN,GRU,LSTM}
+        if hasattr(self, "_flat_weights_names"):
+            self._flat_weights = [
+                self._parameters[wn] for wn in self._flat_weights_names
+            ]
+
         return true_forward(self, *args, **kwargs)
 
     setattr(MonkeyPatched, "forward", patched_forward)
+
+    def flatten_parameters(self):
+        return  # no-op
+
+    # This (hopefully) avoids trouble on GPU with torch.nn.{RNN,GRU,LSTM}
+    if hasattr(module, "flatten_parameters"):
+        setattr(MonkeyPatched, "flatten_parameters", flatten_parameters)
 
     return child_params_offset, fmodule, type(fmodule)
 
