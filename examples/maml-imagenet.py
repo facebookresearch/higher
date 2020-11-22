7623bace2238621c416f0488a52ab6b067a38b40
Xuanyu Zhou <xuanyu_zhou@berkeley.edu>
2020-11-22 18:47:33 +0800

    Update maml-imagenet.py

--- examples/maml-imagenet.py
+++ examples/maml-imagenet.py
@@ -100,7 +100,7 @@ def main():
     #     nn.MaxPool2d(2, 2),
     #     Flatten(),
     #     nn.Linear(64, args.n_way)).to(device)
-    net = iRevNet([2,2,2], [2,2,2], args.n_way, nChannels=[16,64,256], init_ds=2,
+    net = iRevNet([2,2,2], [1,2,2], args.n_way, nChannels=[16,64,256], init_ds=0,
                  dropout_rate=0.1, affineBN=True, in_shape=[3,84,84], mult=4, use_rev_bw=False).to(device)
 
     # We will use Adam to (meta-)optimize the initial parameters
